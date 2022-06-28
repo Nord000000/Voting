314cd653811e7aada3db178ee6892d7a1acd276e
Nord000000 <102154729+Nord000000@users.noreply.github.com>
2022-06-28 22:55:32 +0200

    Update test.js

--- test.js
+++ test.js
@@ -14,17 +14,32 @@ contract ('Voting', accounts => {
     
     let votingInstance;
     
-    describe("1 - Test des Events", function() {
-         before(async function () {
-             votingInstance = await Voting.deployed();
+    describe("1 - Test ", function() {
+         beforeEach(async function () {
+             votingInstance = await Voting.new({from:owner});
+             await votingInstance.addVoter(voterA,{from:owner});
          });
-
-
     
-        it("1.1 - Le voter doit etre enregistre", async () => {
-            const e = await SpaInstance.Add("chien",100, 3, { from: owner });
-            await SpaInstance.Add("chat",100, 3, { from: owner });
-            expectEvent(findEvent,"animalAdded" ,{id: new BN(0)});
+        it("    1.1 - Revert parce que l'enregistrement n'est pas ouvert", async () => {
+            await expectRevert(votingInstance.addVoter(voterB,{from:owner}));            
         });
+
+        it("    1.2 - Revert parce que pas le owner", async () => {
+            await expectRevert(votingInstance.addVoter(voterB,{from:voterA}));            
+        });
+
+        it("    1.3 - Revert parce que deja enregistre", async () => {
+            await expectRevert(votingInstance.addVoter(voterA,{from:owner}));            
+        });
+
+        it("    1.4 - Enregistrer un voter", async () => {
+            await votingInstance.addVoter(voterC,{from:owner});
+            const voter = await votingInstance.getVoter(voterC,{from:voterA});
+            expect(voter.isRegistered).to.be.true;
+        });
+        it("    1.5 - Emettre emit apres le vote", async () => {
+            const findEvent = await votingInstance.setVote(0, { from: voterA });
+            expectEvent(findEvent, "Voté", {voter: voterA, proposalId: new BN(0)});
+        });        
     });
 });
